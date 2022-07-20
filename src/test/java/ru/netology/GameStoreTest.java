eddda1bdefb87266f5dde70e1eab8c4964079cad
Masha Rodionova <rodionovamv@gmai.com>
2022-07-21 01:30:38 +0300

    Add tests for GameStoreTest & rename project in pom.xml

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
@@ -14,5 +16,86 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldAddGame2() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Маша", "Ваня");
+        Game game3 = store.publishGame("Света", "Таня");
+        Game game4 = store.publishGame("Оля", "Дима");
+        assertTrue(store.containsGame(game3));
+    }
+
+    @Test
+    public void shouldContains() {
+
+        //для теста этого метода не хватает геттера
+    }
+
+    @Test
+    public void shouldAddPlayedTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 8);
+        store.addPlayTime("Sveta", 1);
+        store.addPlayTime("Anya", 2);
+        store.addPlayTime("Masha", 2);
+        store.addPlayTime("Nata", 3);
+        store.addPlayTime("Petya", 4);
+
+
+        // не хватает геттера для реализации теста
+
+
+    }
+
+    @Test
+    public void shouldGetMostPlayer() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 8);
+        store.addPlayTime("Sveta", 1);
+        store.addPlayTime("Anya", 2);
+        store.addPlayTime("Nata", 3);
+        store.addPlayTime("Petya", 4);
+        Assertions.assertEquals("Masha", store.getMostPlayer());
+    }
+
+    @Test
+    public void shouldGetMostPlayer2() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 0);
+        store.addPlayTime("Sveta", 0);
+        store.addPlayTime("Anya", 0);
+        store.addPlayTime("Nata", 0);
+        store.addPlayTime("Petya", 0);
+        Assertions.assertEquals(null, store.getMostPlayer());
+    }
+
+    @Test
+    public void shouldGetMostPlayer3() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 7);
+        store.addPlayTime("Sveta", 7);
+        store.addPlayTime("Anya", 7);
+        store.addPlayTime("Nata", 7);
+        store.addPlayTime("Petya", 7);
+        Assertions.assertEquals(null, store.getMostPlayer());
+    }
+
+    @Test
+    public void shouldGetMostPlayer4() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 0);
+        store.addPlayTime("Sveta", 1);
+
+        Assertions.assertEquals("Sveta", store.getMostPlayer());
+    }
+
+    @Test
+    public void shouldGetMostPlayer5() {
+        GameStore store = new GameStore();
+        Assertions.assertEquals(null, store.getMostPlayer());
+    }
+
+
 }
