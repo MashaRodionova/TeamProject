cb8d9d840829f5bc26350081b590e788c516e1e1
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-07-21 18:05:23 +0300

    Creat Test shouildThrowRunTimeException

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -19,6 +19,26 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
     @Test
+    public void shouildSumGenreTwoGames() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Игра1", "Аркады");
+        Game game2 = store.publishGame("Игра2", "Стратегии");
+        Game game3 = store.publishGame("Игра3", "Аркады");
+
+        Player player = new Player("Вася");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 3);
+        player.play(game2, 2);
+        player.play(game3, 3);
+        player.play(game1, 2);
+
+        int expected = 8;
+        int actual = player.sumGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+    @Test
     public void shouildThrowRunTimeException() {
         GameStore store = new GameStore();
         Game game1 = store.publishGame("Игра1", "Аркады");
