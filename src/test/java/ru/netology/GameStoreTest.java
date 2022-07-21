dfc53a3553819fd1c5c6b00c75eb3d71db3ce67e
Masha Rodionova <rodionovamv@gmai.com>
2022-07-21 14:23:36 +0300

    Added tests for contains method, added tests for GetMostPlayer method

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -48,9 +48,40 @@ public class GameStoreTest {
     }
 
     @Test
-    public void shouldContains() {
+    public void shouldContains1() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-        //для теста этого метода не хватает геттера
+        assertTrue(store.containsGame(game));
+    }
+
+    @Test
+    public void shouldContains2() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Маша", "Ваня");
+        Game game3 = store.publishGame("Света", "Таня");
+        Game game4 = store.publishGame("Оля", "Дима");
+        assertTrue(store.containsGame(game3));
+    }
+
+    @Test
+    public void shouldContains3() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Маша", "Ваня");
+        Game game3 = store.publishGame("Света", "Таня");
+        Game game4 = store.publishGame("Оля", "Дима");
+        Game game5 = new Game("Gena", "AntiGena", store);
+        assertFalse(store.containsGame(game5));
+    }
+
+    @Test
+    public void shouldContains4() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Маша", "Ваня");
+        assertTrue(store.containsGame(game1));
     }
 
     @Test
@@ -60,8 +91,6 @@ public class GameStoreTest {
         store.addPlayTime("Sveta", 1);
         store.addPlayTime("Anya", 2);
         store.addPlayTime("Masha", 2);
-        store.addPlayTime("Nata", 3);
-        store.addPlayTime("Petya", 4);
 
 
         // не хватает геттера для реализации теста
@@ -113,6 +142,26 @@ public class GameStoreTest {
 
     @Test
     public void shouldGetMostPlayer5() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 0);
+        store.addPlayTime("Sveta", 0);
+        store.addPlayTime("Anya", 1);
+        store.addPlayTime("Nata", 0);
+        store.addPlayTime("Petya", 0);
+
+        Assertions.assertEquals("Anya", store.getMostPlayer());
+    }
+
+    @Test
+    public void shouldGetMostPlayer6() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Sveta", 1);
+
+        Assertions.assertEquals("Sveta", store.getMostPlayer());
+    }
+
+    @Test
+    public void shouldGetMostPlayer7() {
         GameStore store = new GameStore();
         Assertions.assertEquals(null, store.getMostPlayer());
     }
