1466663c28f0f2a08c961632fad02d33cfe631f0
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-07-21 12:31:27 +0300

    Fix containsGame, close #4

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -13,6 +13,30 @@ public class GameStoreTest {
 
         assertTrue(store.containsGame(game));
     }
+    @Test
+    public void shouldAddGame2() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Маша", "Ваня");
+        Game game3 = store.publishGame("Света", "Таня");
+        Game game4 = store.publishGame("Оля", "Дима");
+        assertTrue(store.containsGame(game3));
+    }
+    @Test
+    public void shouldAddPlayedTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 8);
+        store.addPlayTime("Sveta", 1);
+        store.addPlayTime("Anya", 2);
+        store.addPlayTime("Masha", 2);
+        store.addPlayTime("Nata", 3);
+        store.addPlayTime("Petya", 9);
+        assertEquals("Masha", store.getMostPlayer());
+    }
+
+        // не хватает геттера для реализации теста
+
 
     // другие ваши тесты
 }
